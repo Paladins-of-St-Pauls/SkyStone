5fc6484316d5e8308fa5e07ccc794268d525be95
Phill Crosby <crosphil17@live.trinity.sa.edu.au>
2019-11-16 08:54:31 +1030

    removed imports

--- TeamCode/src/main/java/org/baconeers/stoneStackingMechanism/StoneTongsTest.java
+++ TeamCode/src/main/java/org/baconeers/stoneStackingMechanism/StoneTongsTest.java
@@ -31,12 +31,9 @@ package org.baconeers.stoneStackingMechanism;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 
 /**
