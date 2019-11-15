4ec4d11247fbc9df464b2599aaf882fe40bb41d0
Max Grivell <msgrivellxpt@gmail.com>
2019-11-16 08:39:30 +1030

    Autonomous
    wrote autonomous, phil needs to create a SkyGrab Task to pick up and let go of the skystones

--- TeamCode/src/main/java/org/baconeers/Tasks/Autonomous.java
+++ TeamCode/src/main/java/org/baconeers/Tasks/Autonomous.java
@@ -25,10 +25,23 @@ public class Autonomous extends BaconOpMode {
                 config.backLeftMotor, config.backRightMotor,
                 false);
 
-        tasks.add(new MotorTestTask (this, 1.0, config, 0.3, 0.3, 0.3, 0.3));
-        tasks.add(new MotorTestTask(this, 1.0, config, 0, 1, 0, 0));
-        tasks.add(new MotorTestTask(this, 1.0, config, 0, 0, 1, 0));
-        tasks.add(new MotorTestTask(this, 1.0, config, 0, 0, 0, 1));
+        tasks.add(new DriveXYRTask (this, 0.3175, mecanumDrive, 0, 0.3, 0));
+        tasks.add(new DriveXYRTask (this, 0.289, mecanumDrive, 0, -0.3, 0));
+        tasks.add(new DriveXYRTask (this, 4.272, mecanumDrive, -0.3, 0, 0));
+        tasks.add(new DriveXYRTask (this, 0.26, mecanumDrive, 0, 0.3,0));
+        //(pick up) tasks.add(new SkyGrab (this,{add parameters here}));
+        tasks.add(new DriveXYRTask (this, 0.231, mecanumDrive, 0, -0.3, 0));
+        tasks.add(new DriveXYRTask (this, 4.272, mecanumDrive, 0.3, 0, 0));
+        tasks.add(new DriveXYRTask (this,0.173,mecanumDrive,0, 0.3, 0));
+        //(Let go) tasks.add(new SkyGrab(this, {add parameters here}));
+        tasks.add(new DriveXYRTask(this,0.173, mecanumDrive, 0,0.3,0));
+        tasks.add(new DriveXYRTask (this, 5.408, mecanumDrive, -0.3, 0, 0));
+        tasks.add(new DriveXYRTask (this, 0.26, mecanumDrive, 0, 0.3,0));
+        //(pick up)_tasks.add(new SkyGrab (this, 0.3175, mecanumDrive, 0.3, 0.3, 0.3));
+        tasks.add(new DriveXYRTask (this, 0.231, mecanumDrive, 0, -0.3, 0));
+        tasks.add(new DriveXYRTask (this, 5.408, mecanumDrive, 0.3, 0, 0));
+        tasks.add(new DriveXYRTask (this,0.173,mecanumDrive,0, 0.3, 0));
+        //(Let go) tasks.add(new SkyGrab(this, {add parameters here}));
     }
 
 
