5dbc23391fd32423c7c56a223c65e5a4cd6933f3
Phill Crosby <crosphil17@live.trinity.sa.edu.au>
2019-11-16 11:19:02 +1030

    updated stone tongs servos

--- TeamCode/src/main/java/org/baconeers/stoneStackingMechanism/StoneTongsClass.java
+++ TeamCode/src/main/java/org/baconeers/stoneStackingMechanism/StoneTongsClass.java
@@ -33,15 +33,17 @@ public class StoneTongsClass extends BaconComponent {
 
         double servoPos;
         if (aCurrentState) {
-            servoPos = 0.7;
+//            servoPos = 0.7;
 //            config.gripServo.setPosition(servoPos);
             config.gripServo.setPower(0.5);
             tongStatus = "Down (activated)";
         } else if (bCurrentState){
-            servoPos = 0.0;
+//            servoPos = 0.0;
 //            config.gripServo.setPosition(servoPos);
             config.gripServo.setPower(-0.5);
             tongStatus = "Up (deactivated)";
+        } else {
+            config.gripServo.setPower(0.0);
         }
 
         //aPrevState = aCurrentState;
