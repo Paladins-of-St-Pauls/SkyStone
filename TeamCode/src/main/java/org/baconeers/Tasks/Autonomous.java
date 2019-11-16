472715623159071e3d8bdc39c0e2ef11873dd456
Max Grivell <msgrivellxpt@gmail.com>
2019-11-16 12:34:50 +1030

    auto changes

--- TeamCode/src/main/java/org/baconeers/Tasks/Autonomous.java
+++ TeamCode/src/main/java/org/baconeers/Tasks/Autonomous.java
@@ -12,7 +12,7 @@ import SkystoneDrive.NormalisedMecanumDrive;
 import SkystoneDrive.SkystoneConfiguration;
 
 
-@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "TaskTest")
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "SkyAuto")
 public class Autonomous extends BaconOpMode {
 
     private SkystoneConfiguration config;
@@ -27,34 +27,34 @@ public class Autonomous extends BaconOpMode {
                 config.backLeftMotor, config.backRightMotor,
                 false);
 
-        tasks.add(new DriveXYRTask (this, 0.3175, mecanumDrive, 0, -0.3, 0));
+        tasks.add(new DriveXYRTask (this, 0.3175, mecanumDrive, -0.3, 0, 0));
         tasks.add(new FoundationTask(this,0.5, config, 1));
-        tasks.add(new DriveXYRTask (this, 0.289, mecanumDrive, 0, 0.3, 0));
+        tasks.add(new DriveXYRTask (this, 0.289, mecanumDrive, 0.3, 0, 0));
         tasks.add(new FoundationTask(this, 0.5, config, 0));
-        tasks.add(new DriveXYRTask (this, 4.272, mecanumDrive, 0.3, 0, 0));
-        tasks.add(new DriveXYRTask (this, 0.113, mecanumDrive, 0, -0.3,0));
+        tasks.add(new DriveXYRTask (this, 4.272, mecanumDrive, 0, 0.3, 0));
+        tasks.add(new DriveXYRTask (this, 0.113, mecanumDrive, -0.3, 0,0));
         tasks.add(new DriveXYRTask(this, 0.2 ,mecanumDrive, 0,0,-0.3 ));
-        tasks.add(new DriveXYRTask(this,0.1415, mecanumDrive,0,0.3,0));
-        tasks.add(new DriveXYRTask(this,0.289, mecanumDrive, 0,0.3,0));
+        tasks.add(new DriveXYRTask(this,0.1415, mecanumDrive,0.3,0,0));
+        tasks.add(new DriveXYRTask(this,0.289, mecanumDrive, 0.3,0,0));
         tasks.add(new HarvesterServoTask(this,0.5, config,1));
         tasks.add(new HarvesterMotorTask (this,0.3, config, mecanumDrive, 1,0.3));
-        tasks.add(new DriveXYRTask (this, 0.231, mecanumDrive, 0, -0.3, 0));
-        tasks.add(new DriveXYRTask (this, 4.272, mecanumDrive, 0.3, 0, 0));
-        tasks.add(new DriveXYRTask (this,0.173,mecanumDrive,0, 0.3, 0));
+        tasks.add(new DriveXYRTask (this, 0.231, mecanumDrive, -0.3, 0, 0));
+        tasks.add(new DriveXYRTask (this, 4.272, mecanumDrive, 0, 0.3, 0));
+        tasks.add(new DriveXYRTask (this,0.173,mecanumDrive,0.3, 0, 0));
         tasks.add(new HarvesterReleaseTask(this,0.5, config, 1));
-        tasks.add(new DriveXYRTask(this,0.173, mecanumDrive, 0,-0.3,0));
-        tasks.add(new DriveXYRTask (this, 5.408, mecanumDrive, -0.3, 0, 0));
-        tasks.add(new DriveXYRTask (this, 0.113, mecanumDrive, 0, -0.3,0));
+        tasks.add(new DriveXYRTask(this,0.173, mecanumDrive, -0.3,0,0));
+        tasks.add(new DriveXYRTask (this, 5.408, mecanumDrive, 0, -0.3, 0));
+        tasks.add(new DriveXYRTask (this, 0.113, mecanumDrive, -0.3, 0,0));
         tasks.add(new DriveXYRTask(this, 0.2 ,mecanumDrive, 0,0,-0.3 ));
-        tasks.add(new DriveXYRTask(this,0.1415, mecanumDrive,0,0.3,0));
-        tasks.add(new DriveXYRTask(this,0.289, mecanumDrive, 0,0.3,0));
+        tasks.add(new DriveXYRTask(this,0.1415, mecanumDrive,0.3,0,0));
+        tasks.add(new DriveXYRTask(this,0.289, mecanumDrive, 0.3,0,0));
         tasks.add(new HarvesterServoTask(this,0.5, config,1));
         tasks.add(new HarvesterMotorTask (this,0.3, config, mecanumDrive, 1,0.3));
-        tasks.add(new DriveXYRTask (this, 0.231, mecanumDrive, 0, -0.3, 0));
-        tasks.add(new DriveXYRTask (this, 4.272, mecanumDrive, 0.3, 0, 0));
-        tasks.add(new DriveXYRTask (this,0.173,mecanumDrive,0, 0.3, 0));
+        tasks.add(new DriveXYRTask (this, 0.231, mecanumDrive, -0.3, 0, 0));
+        tasks.add(new DriveXYRTask (this, 4.272, mecanumDrive, 0, 0.3, 0));
+        tasks.add(new DriveXYRTask (this,0.173,mecanumDrive,0.3, 0, 0));
         tasks.add(new HarvesterReleaseTask(this,0.5, config, 1));
-        tasks.add(new DriveXYRTask(this,0.173, mecanumDrive, 0,-0.3,0));
+        tasks.add(new DriveXYRTask(this,0.173, mecanumDrive, -0.3,0,0));
 
         //(Let go) tasks.add(new SkyGrab(this, {add parameters here}));
     }
