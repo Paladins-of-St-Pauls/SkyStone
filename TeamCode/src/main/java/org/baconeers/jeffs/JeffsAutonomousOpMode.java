b2f4ebfc65b7f0f2d79cb82094dc61abca00859c
Caleb Rodgers <24555386+calebrodgers@users.noreply.github.com>
2020-08-05 19:13:23 +0930

    Initial code for Jeff's Challenge.

--- TeamCode/src/main/java/org/baconeers/jeffs/JeffsAutonomousOpMode.java
+++ TeamCode/src/main/java/org/baconeers/jeffs/JeffsAutonomousOpMode.java
@@ -2,46 +2,35 @@ package org.baconeers.jeffs;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-import org.baconeers.Tasks.DriveXYRTask;
-import org.baconeers.Tasks.TankDriveTask;
-import org.baconeers.Tasks.Task;
 import org.baconeers.common.BaconOpMode;
 import org.baconeers.common.GamePadSteerDrive;
-import org.baconeers.common.TankDrive;
 import org.baconeers.testbot.JeffsBotConfiguration;
-
-import java.util.ArrayDeque;
 
 @Autonomous(name = "JeffsAutonomousOpMode")
 public class JeffsAutonomousOpMode extends BaconOpMode {
     private JeffsBotConfiguration config;
-    private TankDrive drive;
-    private ArrayDeque<Task> tasks = new ArrayDeque<>();
+    private GamePadSteerDrive drive;
 
     @Override
     protected void onInit() {
-        config = JeffsBotConfiguration.newConfig(hardwareMap, telemetry);
+       config  = JeffsBotConfiguration.newConfig(hardwareMap, telemetry);
 
-        drive = new TankDrive(this, gamepad1, config.leftMotor, config.rightMotor);
-        tasks.add(new TankDriveTask(this, 1.82, drive, 0.3, 0));
-        tasks.add(new TankDriveTask(this, 1.82, drive, 0, 0.3));
-        tasks.add(new TankDriveTask(this, 3, drive, 1, 1));
-        tasks.add(new TankDriveTask(this, 2  , drive, 1, -1));
+       drive = new GamePadSteerDrive(this, gamepad1, config.leftMotor, config.rightMotor);
+
     }
 
     @Override
     protected void activeLoop() throws InterruptedException {
-        Task currentTask = tasks.peekFirst();
-        if (currentTask == null) {
-            return;
-        }
-        currentTask.run();
-        if (currentTask.isFinished()){
-            tasks.removeFirst();
-        }
-        if (tasks.isEmpty()) {
-            drive.setPower(0, 0);
-            drive.update();
-        }
+        drive.update();
+
+//        if (config.touchSensor.isPressed()) {
+//            telemetry.addLine("Touch pressed");
+//            config.leftMotor.setPower(0.3);
+//            config.rightMotor.setPower(0.0);
+//        } else {
+//            config.leftMotor.setPower(0.0);
+//            config.rightMotor.setPower(0.3);
+//
+//        }
     }
 }
