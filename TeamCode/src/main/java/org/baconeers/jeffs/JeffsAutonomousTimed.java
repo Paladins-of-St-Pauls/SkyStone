818e3e8041a6e69c7059efa21a9c608cad0404f6
Caleb Rodgers <24555386+calebrodgers@users.noreply.github.com>
2020-08-31 11:01:48 +0930

    Jeff's Challenge Autonomous

--- TeamCode/src/main/java/org/baconeers/jeffs/JeffsAutonomousTimed.java
+++ TeamCode/src/main/java/org/baconeers/jeffs/JeffsAutonomousTimed.java
@@ -13,8 +13,8 @@ import org.baconeers.testbot.JeffsBotConfiguration;
 
 import java.util.ArrayDeque;
 
-@Autonomous(name = "JeffsAutonomousOpMode")
-public class JeffsAutonomousOpMode extends BaconOpMode {
+@Autonomous(name = "JeffsAutonomousTimed")
+public class JeffsAutonomousTimed extends BaconOpMode {
     private JeffsBotConfiguration config;
     private TankDrive drive;
     private ArrayDeque<Task> tasks = new ArrayDeque<>();
@@ -26,7 +26,10 @@ public class JeffsAutonomousOpMode extends BaconOpMode {
         config.rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         drive = new TankDrive(this, gamepad1, config.leftMotor, config.rightMotor);
-        tasks.add(new TankDriveTask(this, 20, drive, -0.7, -0.7));
+        tasks.add(new TankDriveTask(this, .2, drive, -0.2, -0.2));
+        tasks.add(new TankDriveTask(this, 6, drive, -0.7, -0.7));
+        tasks.add(new TankDriveTask(this, 2, drive, -0.7, 0));
+        tasks.add(new TankDriveTask(this, 6.5, drive, -0.7, -0.7));
     }
 
     @Override
